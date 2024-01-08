160b7fb0d42e16be545b984e7f123af414e9cbba
ArtusAbelard <tribelha@gmail.com>
2024-01-08 11:29:48 +0100

    add func posture

--- public/js/main.js
+++ public/js/main.js
@@ -11,6 +11,14 @@ class Heros {
         this.force=force;
         this.pv=pv;
         this.posture=posture;
+        this.posture = () => {
+            this.posture=console.log(prompt("choissisez attaque ou defense"));
+            if (this.posture="attaque") {
+                console.log("vous etes en attaque vous infligez 20% plus de dégât");   
+                this.force=(this.force/100)*20;
+                console.log(this.force);
+            }
+        }
     }
 }
 
@@ -19,8 +27,8 @@ let sauron = new Boss ("Sauron",0,0);
 let chronos = new Boss ("Chronos",0,0);
 let lilith = new Boss ("Lilith",0,0);
 
-let artus = new Heros ("artus",0,0,["attaque","defence"])
+let artus = new Heros ("artus",100,0,["attaque","defence"])
 let ariel = new Heros ("ariel",0,0,"")
 let eric = new Heros ("eric",0,0,"")
 
-
+artus.posture()
